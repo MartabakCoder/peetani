7257d94b306c5db7a4d4d96a89239f2c0fd16be7
Rajendra Nohan <rajendranohan4@gmail.com>
2022-01-20 21:19:39 +0700

    initial commit

---
+++ App.js
@@ -0,0 +1,46 @@
+
+import "./App.css";
+import Home from "./pages/home/Home";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import ProfileShow from "./pages/profile/ProfileShow";
+import Timer from "./pages/timer/Timer";
+import Login from "./pages/auth/Login";
+import ProductList from "./pages/productList/ProductList";
+import LayoutAuth from "./LayoutAuth";
+import { createTheme, ThemeProvider } from '@mui/material';
+
+const theme = createTheme({
+  palette: {
+    primary: {
+      main: '#427411',
+      contrastText: '#fff',
+    },
+    secondary: {
+      main: '#639B2F',
+      contrastText: '#fff',
+    },
+    neutral: {
+      main: '#F6F5F5',
+      contrastText: '#393939',
+    },
+  }
+});
+
+function App() {
+  return (
+    <ThemeProvider theme={theme}>
+      <Router>
+          <Route path="/logout" >
+            <Login />
+          </Route>
+          <LayoutAuth path="/profile" component={ProfileShow} />
+          <LayoutAuth exact path="/" component={Home} />
+          <LayoutAuth exact path="/timer" component={Timer} />
+          <LayoutAuth exact path="/history" component={ProductList} />
+      </Router>
+      
+    </ThemeProvider>
+  );
+}
+
+export default App;
